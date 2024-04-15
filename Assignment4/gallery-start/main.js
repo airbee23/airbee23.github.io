266e91d9c42e63ec763678740b87e1b862554f53
Samuel Shihata <samuel.shihata@dcmail.ca>
2024-04-15 19:02:53 -0400

    Worked on Array, and alternative text

--- Assignment4/gallery-start/main.js
+++ Assignment4/gallery-start/main.js
@@ -5,8 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altTexts = ['Closeup of a human eye', 'Some other description', 'Another description'];
 
 /* Looping through images */
 
