e9e8f557d09ba55242a1674b871f82f45fd9d55b
Samuel Shihata <samuel.shihata@dcmail.ca>
2024-04-15 19:08:53 -0400

    Completed the array and alt text for all pictures

--- Assignment4/gallery-start/main.js
+++ Assignment4/gallery-start/main.js
@@ -5,10 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg'];
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
-const altTexts = ['Closeup of a human eye', 'Some other description', 'Another description'];
+const altTexts = ['Closeup of a human eye', 'Some other description', 'Picture of Flowers', 'Egyptian wall iconagraphy', 'Butterfly'];
 
 /* Looping through images */
 
